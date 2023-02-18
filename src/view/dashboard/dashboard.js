0e24566455b79a788d60645ab80c873d16d119e4
francisca <fcaavv@gmail.com>
2023-02-18 10:30:54 -0300

    ajustes css, mostrar posts en el muro

--- src/view/dashboard/dashboard.js
+++ src/view/dashboard/dashboard.js
@@ -1,39 +1,62 @@
-import { logout } from '../../firebase/firebase.js';
+// import { footer } from "../../components/footer.js";
+
+import { addPost, logout } from "../../firebase/firebase.js";
+
+import { newPost } from "../../components/post.js";
+// import { newPost } from "../../components/post.js";
 
 export const dashboard = () => {
-  const viewDashboard = document.createElement('div');
-  viewDashboard.classList.add('post-container');
+  const viewDashboard = document.createElement("div");
+  viewDashboard.classList.add("post-container");
   viewDashboard.innerHTML = `
-<div class= "post-div">
-  <div class="logo-container">
-    <img src="assets/logo.webp"/>
-    <h1>PURRFECT BOOKS</h1>
-  </div>
-  <form class="post-form" id="post-form" >
-  <h4>Nombre</h4>
-  <div class="post-fields">
-    <label for="title"></label>
-  </div>
-  <input type="text" id= "post-title" placeholder="TÃ­tulo del libro">
-  <div >
-    <label for="description"></label>
-  </div>  
-  <div>
-    <textarea id="post-description" placeholder="ReseÃ±a"></textarea>
-  </div>
-  <div >
-    <button class="button-post-save" id="button-post-save">Publicar</button>
-  </div>
-  <button type="button" id="logout-btn">logout</button>
-</div>
+    <div class= "post-div">
+      <div class="logo-container">
+        <img src="assets/logo.webp"/>
+        <h1>PURRFECT BOOKS</h1>
+      </div>
+      <div class="test">
+      <h4>Nombre</h4>
+      </div>
+      <form class="post-form" id="post-form">
+        <div class="post-fields">
+          <label for="post-title"></label>
+          <input type="text" id= "post-title" placeholder="Titulo del libro">
+        </div>
+        <div>
+          <label for="description"></label>
+          <textarea id="post-description" placeholder="Reseña"></textarea>
+        </div>  
+        <div>
+          <button class="button-post-save" id="button-post-save">Publicar</button>
+        </div>
+      </form>
+      <button type="button" id="logout-btn">logout</button>
+      <div id="posts-container"></div>
+    </div>
+  `;
 
-  <div id="post-container"></div>
-</form>`;
+  // ADDING POST
+  const dashboardPost = viewDashboard.querySelector("#button-post-save");
+  dashboardPost.addEventListener("click", (e) => {
+    e.preventDefault();
+    const title = viewDashboard.querySelector("#post-title").value;
+    const description = viewDashboard.querySelector("#post-description").value;
+    if (title === "" || description === "") {
+      alert("Debes completar todos los campos");
+    } else {
+      addPost(title, description).then(() => {
+        viewDashboard.querySelector("#post-form").reset();
+      });
+      newPost();
+    }
+  });
 
-  viewDashboard.querySelector('#logout-btn').addEventListener('click', (e) => {
+  // LOGOUT BUTTON
+  viewDashboard.querySelector("#logout-btn").addEventListener("click", (e) => {
     e.preventDefault();
     logout();
   });
 
+  // viewDashboard.appendChild(footer());
   return viewDashboard;
 };
